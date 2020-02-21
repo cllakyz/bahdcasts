fdbaceffce923a5b3bd2811de6ec1de5fe8bed2e
Celal <cllakyz@hotmail.com>
2020-02-21 15:24:14 +0300

    register fake confirm mail test

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Mail\ConfirmYourEmail;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -80,6 +82,7 @@ class RegisterController extends Controller
      */
     protected function registered(Request $request, $user)
     {
+        Mail::to($user)->send(new ConfirmYourEmail($user));
         return redirect($this->redirectPath());
     }
 }
--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Mail\ConfirmYourEmail;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
 
 class RegistrationTest extends TestCase
 {
@@ -12,6 +14,8 @@ class RegistrationTest extends TestCase
 
     public function test_a_user_has_a_default_username_after_registration()
     {
+        Mail::fake();
+        $this->withoutExceptionHandling();
 
         $this->post('/register', [
             'name' => 'celal akyüz',
@@ -23,5 +27,20 @@ class RegistrationTest extends TestCase
         $this->assertDatabaseHas('users', [
             'username' => str_slug('celal akyüz')
         ]);
+    }
+
+    public function test_an_email_is_sent_to_newly_registered_users()
+    {
+        Mail::fake();
+        $this->withoutExceptionHandling();
+        // register new user
+        $this->post('/register', [
+            'name' => 'celal akyüz',
+            'email' => 'celal@akyuz.com',
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ])->assertRedirect();
+        //assert that a particular email was sent
+        Mail::assertSent(ConfirmYourEmail::class);
     }
 }
