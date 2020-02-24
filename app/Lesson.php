a31f435f007311b6a9f692e49fa9b50d49ae0152
Celal <cllakyz@hotmail.com>
2020-02-24 21:20:20 +0300

    next and previous lessons func

--- app/Lesson.php
+++ app/Lesson.php
@@ -25,4 +25,40 @@ class Lesson extends Model
     {
         return $this->belongsTo(Series::class);
     }
+
+    /**
+     * Get next lesson after $this
+     *
+     * @return Lesson
+     */
+    public function getNextLesson()
+    {
+        $nextLesson = $this->series->lessons()
+            ->where('episode_number', '>', $this->episode_number)
+            ->orderBy('episode_number', 'asc')
+            ->first();
+        if ($nextLesson){
+            return $nextLesson;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get previous lesson before $this
+     *
+     * @return Lesson
+     */
+    public function getPrevLesson()
+    {
+        $prevLesson = $this->series->lessons()
+            ->where('episode_number', '<', $this->episode_number)
+            ->orderBy('episode_number', 'desc')
+            ->first();
+        if ($prevLesson){
+            return $prevLesson;
+        }
+
+        return $this;
+    }
 }
