345aa2d1533238524c8469192876f4057ff1ff2e
Celal <cllakyz@hotmail.com>
2020-02-27 18:53:58 +0300

    edit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Blade::if('hasStartedSeries', function ($series){
             return auth()->user()->hasStartedSeries($series);
         });
